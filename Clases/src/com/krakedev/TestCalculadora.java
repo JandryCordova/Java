1241d907aaa3296a514e43f4e2ef85a32bcd2292
JandryCordova <jandry.torres.03@gmail.com>
2026-03-11 00:20:59 -0500

    Ejercicio metodos Terminado

--- Clases/src/com/krakedev/TestCalculadora.java
+++ Clases/src/com/krakedev/TestCalculadora.java
@@ -6,8 +6,16 @@ public class TestCalculadora {
 		Calculadora calcu=new Calculadora();
 		int ResultadoSuma=calcu.sumar(5, 8);
 		double ResultadoResta=calcu.resta(100, 56);
+		double ResultadoMultiplicar=calcu.multiplicar(10, 9);
+		double ResultadoDividir=calcu.dividir(100, 2);
+		double ResultadoPromedio=calcu.promedio(9, 8, 7);
+		
 		System.out.println("Resultado Suma: "+ResultadoSuma);
 		System.out.println("Resultado Resta: "+ResultadoResta);
+		System.out.println("Resultado Multiplicacion: "+ResultadoMultiplicar);
+		System.out.println("Resultado Division: "+ResultadoDividir);
+		System.out.println("Resultado Promedio: "+ResultadoPromedio);
+		calcu.mostrarResultado();
 	}
 
 }
