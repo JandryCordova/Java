6a00347f782f956ff9118090f2842dafc2185a5b
JandryCordova <jandry.torres.03@gmail.com>
2026-03-11 22:33:17 -0500

    Practica de la clase

---
+++ Clases/src/com/krakedev/TestFecha.java
@@ -0,0 +1,19 @@
+package com.krakedev;
+
+public class TestFecha {
+
+	public static void main(String[] args) {
+		Fecha fecha1 = new Fecha();
+		
+		fecha1.setAnio(2000);
+		fecha1.setMes(12);
+		fecha1.setDia(30);
+		
+		System.out.println(fecha1.getAnio()+"/"+fecha1.getMes()+"/"+fecha1.getDia());
+		System.out.println(fecha1.getAnio());
+		System.out.println(fecha1.getMes());
+		System.out.println(fecha1.getDia());
+
+	}
+
+}
