3c41089cc3918b37b6bbb8e855f6b64e793519c7
JandryCordova <jandry.torres.03@gmail.com>
2026-03-11 20:40:00 -0500

    CalcularPerimetro

--- Clases/src/com/krakedev/TestRectangulo.java
+++ Clases/src/com/krakedev/TestRectangulo.java
@@ -14,9 +14,13 @@ public class TestRectangulo {
 		
 		int AreaR1=r1.calcularArea();
 		int AreaR2=r2.calcularArea();
+		double PerimetroR1=r1.calcularPerimetro();
+		double PerimetroR2=r2.calcularPerimetro();
 		
 		System.out.println("El Area del Rectangulo 1 es: "+AreaR1);
 		System.out.println("El area del rectangulo 2 es: "+AreaR2);
+		System.out.println("El perimetro del rectangulo 1 es: "+PerimetroR1);
+		System.out.println("El perimetro del rectangulo 1 es: "+PerimetroR2);
 
 	}
 
